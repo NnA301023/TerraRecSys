d65e5f0d4a651e5027fedf99d478623bfd11d028
alif_ramadhan <alif@skinmystery.com>
2024-01-26 22:16:49 +0700

    update: strategy file large more than 100mb on streamlit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import joblib
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from sklearn.neighbors import NearestNeighbors
 from src.preprocess import recommend_top_n_product
 
 
@@ -11,12 +12,37 @@ st.set_page_config(page_title="Terra Store Web AI", layout="wide")
 
 @st.cache(allow_output_mutation=True)
 def load_data(dataset_path: str = "./src/agg_table.csv") -> pd.DataFrame:
-    return pd.read_csv(dataset_path)
+    try:
+        return pd.read_csv(dataset_path)
+    except:
+        print("[*] Joining Table on Server...")
+        df_customer = pd.read_csv("../dataset/customer_interactions_synt.csv")
+        df_product  = pd.read_csv("../dataset/product_details_synt.csv")
+        df_purchase = pd.read_csv("../dataset/purchase_history_synt.csv")
+        merged_data = pd.merge(df_product, df_purchase, on='product_id', how='inner')
+        merged_data = pd.merge(merged_data, df_customer, on='customer_id', how='inner')
+        merged_data = merged_data.drop_duplicates().reset_index(drop=True)
+        merged_data.to_csv(dataset_path, index=False)
+        return merged_data
 
 
 @st.cache(allow_output_mutation=True)
 def load_model(model_path: str = "./src/model.pkl") -> object:
-    return joblib.load(model_path)
+    try:
+        return joblib.load(model_path)
+    except:
+        print("[*] Train KNN on Server...")
+        merged_data = pd.read_csv("./src/agg_table.csv")
+        features = ["category", "price", "ratings", "page_views", "time_spent"]
+        X = merged_data[features]
+        map_category = {
+            'Electronics': 1, 'Clothing': 2, 
+            'Home & Kitchen': 3, 'Beauty': 4
+        }
+        X['category'] = X['category'].replace(map_category)
+        knn_model = NearestNeighbors(n_neighbors=5, metric="euclidean")
+        knn_model.fit(X)
+        joblib.dump(knn_model, model_path)
 
 
 def cust_behaviour_viz(df: pd.DataFrame, cust_id: int) -> None:
--- src/preprocess.py
+++ src/preprocess.py
@@ -9,7 +9,26 @@ def recommend_top_n_product(
     map_category: Dict[str, str] = {'Electronics': 1, 'Clothing': 2, 'Home & Kitchen': 3, 'Beauty': 4}
     ) -> List[int]:
     """
-    
+    Recommends the top N products for a given customer based on a KNN model.
+
+    Parameters:
+    - customer_id (int): The ID of the customer for whom recommendations are generated.
+    - merged_data (pd.DataFrame): A DataFrame containing merged data of customer and product features.
+    - knn_model (object): A trained KNN model used for recommendation.
+    - n (int, optional): The number of top products to recommend. Defaults to 5.
+    - features (List[str], optional): A list of feature names used for recommendation. 
+      Defaults to ["category", "price", "ratings", "page_views", "time_spent"].
+    - map_category (Dict[str, str], optional): A dictionary mapping category names to numerical values. 
+      Defaults to {'Electronics': 1, 'Clothing': 2, 'Home & Kitchen': 3, 'Beauty': 4}.
+
+    Returns:
+    - List[int]: A list of product IDs recommended for the given customer.
+
+    Note:
+    - Ensure that the `merged_data` DataFrame contains the necessary features such as 'customer_id', 'product_id', 
+      and features listed in the `features` parameter.
+    - The `knn_model` should be a trained KNN model compatible with scikit-learn's KNeighborsClassifier
+    - The `map_category` parameter maps category names to numerical values for better processing.
     """
     top_n_prods = []
     cust_features = merged_data[merged_data['customer_id']!=customer_id][features]
